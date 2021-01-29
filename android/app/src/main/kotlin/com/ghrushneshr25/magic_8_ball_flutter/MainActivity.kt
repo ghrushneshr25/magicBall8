1881deeee2d01fcb133e1182468d39fc0618620f
Ghrushnesh Rathod <ghrushneshr24@gmail.com>
2021-01-29 22:12:17 +0530

    Final Project

---
+++ android/app/src/main/kotlin/com/ghrushneshr25/magic_8_ball_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ghrushneshr25.magic_8_ball_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      backgroundColor: Colors.blue,
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int random = Random().nextInt(5)+1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: FlatButton(
+            onPressed: (){
+              setState(() {
+                random = Random().nextInt(5)+1;
+              });
+            },
+            child: Image.asset("images/ball$random.png"),
+          )
+      ),
+    );
+  }
+}
